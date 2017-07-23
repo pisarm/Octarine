05482644ba972732163c6e92b6f23960d180371d
Flemming Pedersen <flemming@pisarm.dk>
2017-07-23 23:13:58 +0200

    Refactored 'storage' (added timezone)

--- main/storage.c
+++ main/storage.c
@@ -1,3 +1,29 @@
+/*-
+ * Copyright (c) 2017 Flemming Pedersen
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include "storage.h"
 
 #include "nvs.h"
@@ -5,26 +31,16 @@
 #define STORAGE "storage"
 #define STORAGE_SSID "ssid"
 #define STORAGE_PASSWORD "password"
+#define STORAGE_KEY_TIMEZONE "timezone"
 
-esp_err_t get_wifi_settings(char *ssid, char *password) {
+esp_err_t load(char *storage_key, char *storage_value, size_t length) {
     nvs_handle storage_handle;
     nvs_open(STORAGE, NVS_READONLY, &storage_handle);
-
-    esp_err_t err;
-    size_t length;
-
-    length = SSID_MAX;
-    err = nvs_get_str(storage_handle, STORAGE_SSID, ssid, &length);
-    if (err != ESP_OK) {    
+    
+    esp_err_t status = nvs_get_str(storage_handle, storage_key, storage_value, &length);
+    if (status != ESP_OK) {    
         nvs_close(storage_handle);
-        return err;
-    }
-
-    length = PASSWORD_MAX;
-    err = nvs_get_str(storage_handle, STORAGE_PASSWORD, password, &length);
-    if (err != ESP_OK) {
-        nvs_close(storage_handle);
-        return err;
+        return status;
     }
 
     nvs_close(storage_handle);
@@ -32,20 +48,65 @@ esp_err_t get_wifi_settings(char *ssid, char *password) {
     return ESP_OK;
 }
 
-esp_err_t set_wifi_settings(char *ssid, char *password) {
+esp_err_t store(char *storage_key, char *storage_value) {
     nvs_handle storage_handle;
     nvs_open(STORAGE, NVS_READWRITE, &storage_handle);
 
-    esp_err_t err;
-
-    err = nvs_set_str(storage_handle, STORAGE_SSID, ssid);
-    if (err != ESP_OK) {
-        return err;
+    esp_err_t status = nvs_set_str(storage_handle, storage_key, storage_value);
+    if (status != ESP_OK) {
+        return status;
     }
 
-    err = nvs_set_str(storage_handle, STORAGE_PASSWORD, password);
-    if (err != ESP_OK) {
-        return err;
+    nvs_commit(storage_handle);
+    nvs_close(storage_handle);
+
+    return ESP_OK;
+}
+
+esp_err_t load_timezone(char *timezone) {
+    size_t length = TIMEZONE_MAX;    
+    return load(STORAGE_KEY_TIMEZONE, timezone, length);
+}
+
+esp_err_t store_timezone(char *timezone) {
+    return store(STORAGE_KEY_TIMEZONE, timezone);
+}
+
+esp_err_t load_wifi_credentials(char *ssid, char *password) {
+    nvs_handle storage_handle;
+    nvs_open(STORAGE, NVS_READONLY, &storage_handle);
+
+    size_t length = SSID_MAX;
+    esp_err_t status = nvs_get_str(storage_handle, STORAGE_SSID, ssid, &length);
+    if (status != ESP_OK) {    
+        nvs_close(storage_handle);
+        return status;
+    }
+
+    length = PASSWORD_MAX;
+    status = nvs_get_str(storage_handle, STORAGE_PASSWORD, password, &length);
+    if (status != ESP_OK) {
+        nvs_close(storage_handle);
+        return status;
+    }
+
+    nvs_close(storage_handle);
+
+    return ESP_OK;
+}
+
+esp_err_t store_wifi_credentials(char *ssid, char *password) {
+    nvs_handle storage_handle;
+    nvs_open(STORAGE, NVS_READWRITE, &storage_handle);
+
+    esp_err_t status = nvs_set_str(storage_handle, STORAGE_SSID, ssid);
+    if (status != ESP_OK) {
+        return status;
+    }
+
+    status = nvs_set_str(storage_handle, STORAGE_PASSWORD, password);
+    if (status != ESP_OK) {
+        return status;
     }
 
     nvs_commit(storage_handle);
--- main/storage.h
+++ main/storage.h
@@ -1,12 +1,42 @@
+/*-
+ * Copyright (c) 2017 Flemming Pedersen
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #ifndef STORAGE_H
 #define STORAGE_H
 
 #include "esp_err.h"
 
+#define TIMEZONE_MAX 32
 #define SSID_MAX 32
 #define PASSWORD_MAX 64
 
-esp_err_t get_wifi_settings(char *ssid, char *password);
-esp_err_t set_wifi_settings(char *ssid, char *password);
+esp_err_t load_timezone(char *timezone);
+esp_err_t store_timezone(char *timezone);
+
+esp_err_t load_wifi_credentials(char *ssid, char *password);
+esp_err_t store_wifi_credentials(char *ssid, char *password);
 
 #endif
